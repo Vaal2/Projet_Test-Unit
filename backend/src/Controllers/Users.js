0c7c70e55b9f2d223effa396c31cac3b36084294
Vaal2 <dragnir19@gmail.com>
2021-12-29 23:38:51 +0100

    merging

--- backend/src/Controllers/Users.js
+++ backend/src/Controllers/Users.js
@@ -6,30 +6,38 @@ export const fetchAll = () => {
 		new Users().fetchAll().then(res => {
 			resolve(res.toJSON())
 		}).catch(err => {
-			console.log('fetch all', err);
 		})
 	})
 }
 
 export const create = (usersArray) => {
 	return new Promise((resolve, fail) => {
+		let count = 0;
 		usersArray.map((item) => {
-			Users.forge(item).save().then(res => {
-				console.log('créé');
-			}).catch(err => {
-				console.log(err);
-			})
-		})
-		resolve()
+			let loaded = false;
+
+			while (!loaded) {
+				Users.forge(item).save().then(res => {
+					count++;
+	
+					if(count === usersArray.length) {
+						resolve('good')
+						loaded = true
+					}
+				})
+			}
+		});
 	})
 }
 
 export const destroyAll = (users) => {
-	Knex.raw(
-		'DELETE FROM users'
-	).then(res => {
-		console.log('deleted');
-	}).catch(err => {
-		console.log(err);
+	return new Promise((resolve, fail) => {
+		Knex.raw(
+			'DELETE FROM users'
+		).then(res => {
+			resolve('good')
+		}).catch(err => {
+			fail('error')
+		})
 	})
 }
